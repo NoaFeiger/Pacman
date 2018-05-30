cdbed7de37d1c86a03766ecd2dba40547584ebc5
sagisaa <sagisaa@post.bgu.ac.il>
2018-05-30 19:15:23 +0300

    Tiny fix

--- src/Visitors/Capsule.java
+++ src/Visitors/Capsule.java
@@ -2,11 +2,11 @@ package Visitors;
 
 import javax.swing.*;
 
-public class Capsul {
+public class Capsule {
     private int points;
     private int quantity;
     private ImageIcon img_capsul;
-    public Capsul(int points,int quantity, String path){
+    public Capsule(int points,int quantity, String path){
         this.points=points;
         this.quantity=quantity;
         this.img_capsul=new ImageIcon(path);
