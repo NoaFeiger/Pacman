d189f9139cff7b90de1e27feea91e7e15fd59029
sagisaa <sagisaa@post.bgu.ac.il>
2018-05-31 13:06:15 +0300

    Text on the board
    It's not showing up so well, I need to fix this

--- src/Main/GameControl.java
+++ src/Main/GameControl.java
@@ -17,6 +17,10 @@ public class GameControl implements ActionListener {
     private int freeze;
     private Timer timer;
     private final int delay = 200;
+    private JLabel pointsL;
+    private JLabel lifesL;
+    private JPanel statusP;
+    private Color curBackground;
 
     public GameControl(JFrame frame){
         this.frame = frame;
@@ -24,11 +28,28 @@ public class GameControl implements ActionListener {
         this.points = 0;
         this.level = 0;
         this.freeze = 0;
+        this.curBackground = new Color(3,3,99);
+        this.statusP = new JPanel(new GridLayout(2,1));
+        this.pointsL = new JLabel("Points: 0",SwingConstants.CENTER);
+        this.pointsL.setForeground(Color.WHITE);
+        this.pointsL.setFont(new Font("",Font.BOLD,20));
+        this.pointsL.setSize(200,200);
+        this.pointsL.setHorizontalAlignment(0);
+        this.statusP.add(pointsL);
+        this.lifesL = new JLabel("Lifes: 0",SwingConstants.CENTER);
+        this.lifesL.setForeground(Color.WHITE);
+        this.lifesL.setFont(new Font("",Font.BOLD,20));
+        this.statusP.add(lifesL);
+        this.statusP.setBackground(this.curBackground);
+        this.lifesL.setSize(100,200);
+        this.lifesL.setHorizontalAlignment(0);
+        this.frame.add(statusP,BorderLayout.EAST);
+        this.frame.getContentPane().setBackground(this.curBackground);
     }
     public void startGame(){
         this.level = 1;
         this.levelGame = new LevelGame(frame,this.level,"BoardLevel" + this.level);
-        this.levelGame.setBackground(new Color(3,3,99));
+        this.levelGame.setBackground(this.curBackground);
         frame.add(this.levelGame);
         frame.setBackground(Color.BLACK);
         frame.repaint();
@@ -40,7 +61,10 @@ public class GameControl implements ActionListener {
         this.level++;
         frame.remove(this.levelGame);
         this.levelGame = new LevelGame(frame,this.level,"BoardLevel" + this.level);
-        this.levelGame.setBackground(new Color(5,5,160));
+        this.curBackground = new Color(5,5,160);
+        this.statusP.setBackground(this.curBackground);
+        this.levelGame.setBackground(curBackground);
+        this.frame.getContentPane().setBackground(this.curBackground);
         frame.add(this.levelGame);
         frame.repaint();
         frame.revalidate();
@@ -51,7 +75,6 @@ public class GameControl implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==timer) {
-            System.out.println(this.points);
             StatusChange statusChange = null;
             if(this.freeze==0)
                 statusChange = this.levelGame.move();
@@ -71,6 +94,8 @@ public class GameControl implements ActionListener {
                 timer.stop();
                 nextLevel();
             }
+            pointsL.setText("Points: " + this.points);
+            lifesL.setText("Lifes: " + this.lifes);
         }
     }
 }
