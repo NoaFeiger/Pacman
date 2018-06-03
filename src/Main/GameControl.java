79ec30b4524477d23a4c2e5a143c654864b1d9a0
NoaFeiger <noafeiger29@gmail.com>
2018-06-03 14:29:58 +0300

    CorrectStartLevel

--- src/Main/GameControl.java
+++ src/Main/GameControl.java
@@ -12,6 +12,7 @@ public class GameControl implements ActionListener {
     JFrame frame;
     private LevelGame levelGame;
     private int lifes;
+    private boolean change_level;
     private int points;
     private int level;
     private int freeze;
@@ -22,34 +23,37 @@ public class GameControl implements ActionListener {
     private JPanel statusP;
     private Color curBackground;
     private JPanel main_panel;
-    public GameControl(JFrame frame, JPanel main_panel){
+
+    public GameControl(JFrame frame, JPanel main_panel) {
         this.frame = frame;
         this.lifes = 3;
+        this.change_level = true;
         this.points = 0;
-        this.main_panel=main_panel;
+        this.main_panel = main_panel;
         this.level = 0;
         this.freeze = 0;
-        this.curBackground = new Color(3,3,99);
-        this.statusP = new JPanel(new GridLayout(2,1));
-        this.pointsL = new JLabel("Points: 0",SwingConstants.CENTER);
+        this.curBackground = new Color(3, 3, 99);
+        this.statusP = new JPanel(new GridLayout(2, 1));
+        this.pointsL = new JLabel("Points: 0", SwingConstants.CENTER);
         this.pointsL.setForeground(Color.WHITE);
-        this.pointsL.setFont(new Font("",Font.BOLD,20));
-        this.pointsL.setSize(200,200);
+        this.pointsL.setFont(new Font("", Font.BOLD, 20));
+        this.pointsL.setSize(200, 200);
         this.pointsL.setHorizontalAlignment(0);
         this.statusP.add(pointsL);
-        this.lifesL = new JLabel("Lifes: 0",SwingConstants.CENTER);
+        this.lifesL = new JLabel("Lifes: 0", SwingConstants.CENTER);
         this.lifesL.setForeground(Color.WHITE);
-        this.lifesL.setFont(new Font("",Font.BOLD,20));
+        this.lifesL.setFont(new Font("", Font.BOLD, 20));
         this.statusP.add(lifesL);
         this.statusP.setBackground(this.curBackground);
-        this.lifesL.setSize(100,200);
+        this.lifesL.setSize(100, 200);
         this.lifesL.setHorizontalAlignment(0);
-        this.frame.add(statusP,BorderLayout.EAST);
+        this.frame.add(statusP, BorderLayout.EAST);
         this.frame.getContentPane().setBackground(this.curBackground);
     }
-    public void startGame(){
+
+    public void startGame() {
         this.level = 1;
-        this.levelGame = new LevelGame(frame,this.level,"BoardLevel" + this.level);
+        this.levelGame = new LevelGame(frame, this.level, "BoardLevel" + this.level);
         this.levelGame.setBackground(this.curBackground);
         frame.add(this.levelGame);
         frame.setBackground(Color.BLACK);
@@ -58,11 +62,13 @@ public class GameControl implements ActionListener {
         timer = new Timer(delay, this);
         timer.start();
     }
-    public void nextLevel(){
-        this.level++;
+
+    public void nextLevel(boolean change) {
+        if (change == true)
+            this.level++;
         frame.remove(this.levelGame);
-        this.levelGame = new LevelGame(frame,this.level,"BoardLevel" + this.level);
-        this.curBackground = new Color(5,5,160);
+        this.levelGame = new LevelGame(frame, this.level, "BoardLevel" + this.level);
+        this.curBackground = new Color(5, 5, 160);
         this.statusP.setBackground(this.curBackground);
         this.levelGame.setBackground(curBackground);
         this.frame.getContentPane().setBackground(this.curBackground);
@@ -75,36 +81,41 @@ public class GameControl implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==timer) {
+        if (e.getSource() == timer) {
             StatusChange statusChange = null;
-            for(TimerListener t : LevelGame.monsters)
-            {
+            for (TimerListener t : LevelGame.monsters) {
                 t.action();
             }
-            if(this.freeze==0)
+            if (this.freeze == 0)
                 statusChange = this.levelGame.move();
             else
                 this.freeze--;
-            if (statusChange!=null){
-                this.points = this.points+statusChange.getPoints();
-                this.lifes = this.lifes+statusChange.getLifes();
+            if (statusChange != null) {
+                this.points = this.points + statusChange.getPoints();
                 this.freeze = statusChange.getFreezeTime();
+                if ((statusChange.getLifes() + this.lifes) != this.lifes) {
+                    this.lifes = this.lifes + statusChange.getLifes();
+                    if (this.lifes != 0) {
+                        timer.stop();
+                        nextLevel(false);
+                    }
+                }
+                if (this.lifes == 0) {
+                    timer.stop();
+                    LastPage last_page = new LastPage(frame, main_panel, this.points);
+                    frame.remove(this.levelGame); // move to the game page
+                    frame.add(last_page);
+                    frame.repaint();
+                    frame.revalidate();
+                }
+                if (this.points >= 500 & this.level < 2) {
+                    System.out.println("Next Level");
+                    timer.stop();
+                    nextLevel(true);
+                }
+                pointsL.setText("Points: " + this.points);
+                lifesL.setText("Lifes: " + this.lifes);
             }
-            if(this.lifes==0) {
-                timer.stop();
-                LastPage last_page=new LastPage(frame,main_panel,this.points);
-                frame.remove(this.levelGame); // move to the game page
-                frame.add(last_page);
-                frame.repaint();
-                frame.revalidate();
-            }
-            if(this.points>=500 & this.level<2){
-                System.out.println("Next Level");
-                timer.stop();
-                nextLevel();
-            }
-            pointsL.setText("Points: " + this.points);
-            lifesL.setText("Lifes: " + this.lifes);
         }
     }
 }
