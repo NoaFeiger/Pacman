ecb5d2670f662ff18bda5f7d1b3b38e8f818444c
sagisaa <sagisaa@post.bgu.ac.il>
2018-06-15 00:22:12 +0300

    IMAGES

--- src/Visitors/AngryPacman.java
+++ src/Visitors/AngryPacman.java
@@ -2,7 +2,7 @@ package Visitors;
 
 public class AngryPacman extends Pacman implements Visited {
     public AngryPacman(int x, int y, int lives, int score) {
-        super(x, y, "angry_pacman.png", lives, score);
+        super(x, y, "angry_pacman_open.png", "angry_pacman_close.png", lives, score);
     }
     @Override
     public StatusChange accept(Visitor ghost) {
